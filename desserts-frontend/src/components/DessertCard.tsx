7342a8934b96e34107fc4d32ff58c0282057cc14
Géczi Kornél <hungarokornel@gmail.com>
2024-09-18 11:53:40 +0200

    2024.09.18

---
+++ desserts-frontend/src/components/DessertCard.tsx
@@ -0,0 +1,17 @@
+export type DessertCardProp = {
+  name: string;
+  category: string;
+  price: number;
+  image?: {
+    thumnbail?: string;
+    mobile?: string;
+    tablet?: string;
+    desktop: string;
+  };
+};
+
+const DessertCard = (props: DessertCardProp) => {
+  return <div>{props.name}</div>;
+};
+
+export default DessertCard;
